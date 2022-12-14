fbba9ee62694d09741caf166158e15eba2b34a52
Diego Pacheco <wdpg1@gcloud.ua.es>
2022-12-14 19:43:15 +0100

    Un cambio para generar un conflicto y adelantarme a la rama

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    cout << "Primer Programa en C++" << endl;
+    cout << "Mi nombre es Diego y este es mi primer Programa en C++" << endl;
     cout << "Hola Alicante" << endl;
     cout << "Adios Alicante" << endl;
     cout << "Se ha terminado el programa" << endl;
