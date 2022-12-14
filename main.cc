df177b687d5ed8923f461f725883a0cee4bb8bca
Diego Pacheco <wdpg1@gcloud.ua.es>
2022-12-14 19:44:59 +0100

    Cambios en la rama y provocacion de conflicto a proposito

--- main.cc
+++ main.cc
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main(){
-    cout << "Primer Programa en C++" << endl;
+    cout << "Mi Primer Programa en C++" << endl;
     cout << "Hola Alicante" << endl;
-    cout << "Adios Alicante" << endl;
+    cout << "Adios Alicante y Hasta Nunca :(" << endl;
     cout << "Se ha terminado el programa" << endl;
     return 0;
 }
