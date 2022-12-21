734966225f57bfd20a88adfbf6e084bfa0f99daa
Diego Pacheco <wdpg1@gcloud.ua.es>
2022-12-21 19:46:34 +0100

    fallo solucionado

--- main.cc
+++ main.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     cout << "Mi nombre es Diego y este es mi primer Programa en C++" << endl;
-    cout <<   Hola Alicante" << endl;
+    cout << "  Hola Alicante" << endl;
     cout << "  Adios Alicante y Hasta Nunca :(" << endl;
     cout << "Se ha terminado el programa. " << endl;
     return 0;
